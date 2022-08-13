b11a789e5c2a52959d99fdc8b50b62e81de59c11
Petra <pasztor.petra1998@gmail.com>
2022-08-13 19:17:49 +0200

    js függvények

--- main.js
+++ main.js
@@ -73,6 +73,52 @@ const removeAllClickListeners = () => {
     })
 }
 
+// checkValues: végigmegy a kapott tömb sorain,
+// és a sor minden EGYES elemének értéke esetében megvizsgálja, hogy az 0 vagy X. 
+// Ha a sor minden egyes eleme 0 vagy X, akkor a 0 vagy az X győzött
+
+const checkValues = (array) => array.map(row => {/*Ide írd a kódot!*/})
+    .indexOf(true) !== -1;
+
+    //Ha true-t kapunk visza adott sorra, akkor 
+    //annak indexét vizsgálva nem kaphatunk -1-et.
+    //Azaz az elem benne van a tömbben.
+    //*/
+
+    const checkColumnValues = () => 
+    checkValues(matrix.map((array, i) => 
+    array.map((item, j) => matrix[j][i])));
+
+    const checkDiagonalValues = () =>
+    checkValues([
+    matrix.map((array, i) => matrix[i][i]),
+    matrix.map((array, i) => matrix[i][matrix[i].length - i - 1])
+    ]);
+
+    //Miután az első függvénnyel leellenőriztük a sorok tartalmát,
+    //a fentiekkel megvizsgáljuk az oszlopok és az átlókban lévő mezők tartalmát.
+    //Ez azért fontos, mert így tudhatjuk meg, 
+    //hogy lesz-e három azonos jel egymás mellett/alatt/átlósan.
+
+// checkWinner: 
+
+const checkWinner = () => {},
+
+//setMessage: kiválasztja a message osztályú elemet, és az üzenetet állítja be a div tartalmának
+
+const setMessage = (message) => { 
+    const message = document.querySelectorAll(".message");
+    message.textContent = message; 
+}
+
+//startGame: függvényeket hív meg
+
+const startGame = () => {
+    initState();
+    addClickListener();
+    newGame();
+}
+
 // newGame: gomb kiválasztása és kattintásra függvények meghívása
 
 const newGame = () => {
